7ee66e6cdd5c0a2586fd0ff420c0204015c0a664
aboxofsox <idkwthfml@gmail.com>
2023-12-10 02:27:20 -0600

    ➕ combine args; 🛠️ pwsh build script

--- cmd/combine.go
+++ cmd/combine.go
@@ -7,7 +7,12 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var all bool
+var paths []string
+
 func init() {
+	combine.Flags().BoolVarP(&all, "all", "a", false, "Combine all .gitignore files in the current directory")
+	combine.Flags().StringSliceVarP(&paths, "paths", "p", []string{}, "Combine .gitignore files at the specified paths")
 	rootCmd.AddCommand(combine)
 }
 
@@ -15,9 +20,20 @@ var combine = &cobra.Command{
 	Use:   "combine",
 	Short: "Combine multiple .gitignore files into one",
 	Run: func(cmd *cobra.Command, args []string) {
-		err := iggy.CombineAll()
-		if err != nil {
-			log.Fatal(err)
+		if all {
+			err := iggy.CombineAll()
+			if err != nil {
+				log.Fatal(err)
+			}
+			return
+		}
+
+		if len(paths) > 0 {
+			err := iggy.Combine(paths...)
+			if err != nil {
+				log.Fatal(err)
+			}
+			return
 		}
 	},
 }
